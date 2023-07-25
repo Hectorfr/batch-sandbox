748b63ec40cdaabef064610dacee93d17ae12301
Hector Freard <66312947+Hectorfr@users.noreply.github.com>
2023-07-25 14:12:00 +0200

    show the key availability and password if possible

--- wifi-key-finder/wifi-passwords.bat
+++ wifi-key-finder/wifi-passwords.bat
@@ -5,6 +5,8 @@ netsh wlan show profile
 
 set /p profile=Which of the profiles which you want to know the password?
 
+netsh wlan show profile "%profile%" key=clear | find "Security key"
+echo --------------------------------------------------------------
 netsh wlan show profile "%profile%" key=clear | find "Key Content"
 pause
 
